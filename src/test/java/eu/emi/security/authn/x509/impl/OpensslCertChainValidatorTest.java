f54afeb85b07dbc4db1c57b9e157e36c09d006a0
Krzysztof Benedyczak <kb@unity-idm.eu>
2026-08-21 16:27:25 +0200

    Select native OCSP responders deterministically

--- src/test/java/eu/emi/security/authn/x509/impl/OpensslCertChainValidatorTest.java
+++ src/test/java/eu/emi/security/authn/x509/impl/OpensslCertChainValidatorTest.java
@@ -578,7 +578,7 @@ public class OpensslCertChainValidatorTest
 	}
 
 	@Test
-	public void shouldKeepMultipleDiscoveredRespondersOnCompatibilityPath()
+	public void shouldUseFirstDiscoveredResponderOnNativePath()
 			throws Exception {
 		CA rootCA = given(aCertificateAuthority()
 				.selfSigned()
@@ -601,7 +601,9 @@ public class OpensslCertChainValidatorTest
 
 		assertThat(result.isValid(), is(false));
 		assertThat(result.getPrimaryError().getErrorCode(),
-				is(ValidationErrorCode.ocspResponderQueryError));
+				is(ValidationErrorCode.PKIX_FAILURE));
+		assertThat(result.getPrimaryError().getStage(),
+				is(ValidationStage.REVOCATION));
 	}
 
 	private OCSPResponder startMalformedOCSPResponder(X509Certificate certificate)
